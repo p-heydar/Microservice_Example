b772056d684ca0bbd23846ffbf9a2749856a6b85
Pedram <Pedram@DESKTOP-UVQBME2>
2024-05-31 17:18:45 +0330

    feat: Add Product Domain Model

---
+++ Src/Services/Catalog/Catalog.API/Models/Catalogs/Product.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Models.Common;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Catalog.API.Models.Catalogs;
+
+public class Product:BaseEntity<Guid>
+{
+    public string Name { get; set; } = default!;
+    public List<string> Categories { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; } = decimal.Zero;
+}
