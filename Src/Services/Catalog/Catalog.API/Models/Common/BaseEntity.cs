085da9e60c8546f5dedbc7a5bf423e90c30d9fd9
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-01 22:12:13 +0330

    feat: Add Configuration In BaseEntity

--- Src/Services/Catalog/Catalog.API/Models/Common/BaseEntity.cs
+++ Src/Services/Catalog/Catalog.API/Models/Common/BaseEntity.cs
@@ -1,7 +1,14 @@
 ﻿namespace Catalog.API.Models.Common;
 
 public class BaseEntity<Type>
+    where Type: notnull
 {
     public Type Id { get; set; }
-    public DateTime InsertDate { get; set; }
+    public DateTime InsertDate { get; set; } = DateTime.Now;
+}
+
+public class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime InsertDate { get; set; } = DateTime.Now;
 }
