45434d2eb809ccc298f33c93511aff0a5f5fd391
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-01 22:12:38 +0330

    Feat: Modify CreateProductEndpoint Fro Carter ...

---
+++ Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public sealed record CreateProductRequest(string name, List<string> categories,
+        string description, string imageFile, decimal price);
+
+public sealed record CreateProductResponse(Guid id);
+
+public class CreateProductEndpoint
+{
+}
