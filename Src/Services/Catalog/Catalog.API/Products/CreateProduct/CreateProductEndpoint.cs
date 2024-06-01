0af8d9c085ffcedcf9a0585ea7de91b8e92c3e50
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-01 23:49:48 +0330

    feat: Add EndPoint For CreateProduct

--- Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,4 @@
-﻿using Carter;
-
+﻿
 namespace Catalog.API.Products.CreateProduct;
 
 public sealed record CreateProductRequest(string name, List<string> categories,
@@ -11,6 +10,21 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/Products",
+            async (CreateProductRequest request, ISender sender)
+            =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/Products/{response.id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(statusCode: StatusCodes.Status201Created)
+        .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
