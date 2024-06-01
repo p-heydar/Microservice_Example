a3f15812e73a309d8d27bb6f7466fe15d72907f9
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-01 23:50:15 +0330

    Feat: Add Configuration For Marten In Program.cs

--- Src/Services/Catalog/Catalog.API/Program.cs
+++ Src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,20 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+
+builder.Services.AddMarten(configuration =>
+{
+    configuration.Connection(builder.Configuration.GetConnectionString("DataBase")!);
+}).UseLightweightSessions();
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
