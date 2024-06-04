46c4ea6e171c15ccadaed509717cc6f04932f323
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-04 15:50:50 +0330

    Feat: Add Get All Product Actions

---
+++ Src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+using Catalog.API.Models.Catalogs;
+
+namespace Catalog.API.Products.GetAllProducts;
+
+public sealed record GetAllProductsResponse(IImmutableList<Product> Products);
+
+internal sealed class GetAllProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetAllProductQuery());
+            var response = result.Adapt<GetAllProductsResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetAllProducts")
+        .WithDescription("GetAllProducts")
+        .Produces<GetAllProductsResponse>(statusCode: StatusCodes.Status200OK)
+        .ProducesProblem(statusCode: StatusCodes.Status500InternalServerError)
+        .WithSummary("GetAllProducts");
+    }
+}
---
+++ Src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+using BuildingBlocks.CQRS.Query;
+using Catalog.API.Models.Catalogs;
+using Marten;
+
+namespace Catalog.API.Products.GetAllProducts;
+
+public sealed record GetAllProductQuery():IQuery<GetAllProductResult>;
+
+public sealed record GetAllProductResult(IImmutableList<Product> Products);
+
+internal sealed class GetAllProductHandler(IDocumentSession session) : IQueryHandler<GetAllProductQuery, GetAllProductResult>
+{
+    public async Task<GetAllProductResult> Handle(GetAllProductQuery query,
+        CancellationToken cancellationToken)
+    {
+        var getAllProduct = session
+            .Query<Product>()
+            .ToImmutableList();
+
+        var response = getAllProduct
+            .Adapt<GetAllProductResult>();
+
+        return response;
+    }
+}
