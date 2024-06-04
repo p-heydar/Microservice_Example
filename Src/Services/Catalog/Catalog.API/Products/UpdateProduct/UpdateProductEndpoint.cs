f6eea8ac7c69e83283e7e16d710f13a4571a3926
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-04 15:51:31 +0330

    Feat: Add Update Product Actions

---
+++ Src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.API.Models.Catalogs;
+using Catalog.API.Products.GetProductsByCategory;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public sealed record UpdateProductRequest(Product Product);
+
+public sealed record UpdateProductResponse(bool IsSuccess);
+
+internal sealed class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/Products", async ([FromBody]UpdateProductRequest request,
+            ISender sender) =>
+        {
+            var convertToCommand = request.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(convertToCommand);
+
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithDisplayName("UpdateProduct")
+        .WithSummary("Update Product")
+        .Produces<UpdateProductResponse>(statusCode: StatusCodes.Status200OK)
+        .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+        .WithDescription("Update Product");
+    }
+}
---
+++ Src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,52 @@
+﻿using BuildingBlocks.CQRS.Command;
+
+using Catalog.API.Exceptions.Products;
+using Catalog.API.Models.Catalogs;
+
+using Marten;
+using Marten.Linq.SoftDeletes;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public sealed record UpdateProductCommand(Product Product) : ICommand<UpdateProductResult>;
+
+
+public sealed record UpdateProductResult(bool IsSuccess);
+
+
+internal class UpdateProductHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if ((command.Product.Id.Equals(Guid.Empty)))
+                throw new EmptyGuidException();
+
+            var findProductById = await session.Query<Product>()
+                .FirstOrDefaultAsync(product => product.Id == command.Product.Id)
+                    ?? throw new ProductNotFoundException();
+
+            findProductById.Name = command.Product.Name!;
+            findProductById.Description = command.Product.Description!;
+            findProductById.ImageFile = command.Product.ImageFile!;
+            findProductById.Price = command.Product.Price!;
+            findProductById.Categories = command.Product.Categories!;
+
+            session.Update(findProductById!);
+            await session.SaveChangesAsync();
+
+            return new UpdateProductResult(true);
+        }
+
+        catch (Exception ex)
+        {
+            return new UpdateProductResult(false);
+        }
+
+        finally
+        {
+            await session.DisposeAsync();
+        }
+    }
+}
