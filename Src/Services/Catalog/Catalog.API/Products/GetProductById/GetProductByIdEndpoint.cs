20e3f1b05ca0d39f5cf4821a15e0674c905f0793
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-04 15:51:03 +0330

    Feat: Add Get Product By Id Actions

---
+++ Src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Models.Catalogs;
+
+namespace Catalog.API.Products.GetProductById;
+
+public sealed record GetProductByIdResponse(Product product);
+
+internal sealed class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("$/Products/{Id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(Id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithDisplayName("GetProductById")
+        .WithSummary("Get Product By Id")
+        .Produces<GetProductByIdResponse>(statusCode: StatusCodes.Status200OK)
+        .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+        .WithDescription("Get Product By Id");
+    }
+}
---
+++ Src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS.Query;
+
+using Catalog.API.Exceptions.Products;
+using Catalog.API.Models.Catalogs;
+
+using Marten;
+
+namespace Catalog.API.Products.GetProductById;
+
+public sealed record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public sealed record GetProductByIdResult(Product product);
+
+internal sealed class GetProductByIdHandler(IDocumentSession session) :
+    IQueryHandler<GetProductByIdQuery,
+        GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        var findProductById = await session.Query<Product>()
+            .FirstOrDefaultAsync(x => x.Id == query.Id, cancellationToken)
+                ?? throw new ProductNotFoundException();
+
+        var result = findProductById.Adapt<GetProductByIdResult>();
+
+        return result;
+    }
+}
