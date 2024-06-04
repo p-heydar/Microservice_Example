27d3e78874a2d5c1e4437391a6316b29522b3835
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-04 15:50:36 +0330

    Feat: Add Delete Product Action

---
+++ Src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+
+public sealed record DeleteProductResponse(bool IsSuccess);
+
+internal sealed class DeleteProductEndpoint(IDocumentSession session) : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/Product/{Id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(Id));
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .WithDescription("DeleteProduct By Id")
+        .WithSummary("Delete Product")
+        .Produces<DeleteProductResponse>(statusCode: StatusCodes.Status200OK)
+        .ProducesProblem(statusCode: StatusCodes.Status400BadRequest);
+    }
+}
---
+++ Src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,42 @@
+﻿using BuildingBlocks.CQRS.Command;
+
+using Catalog.API.Exceptions.Products;
+using Catalog.API.Models.Catalogs;
+
+using Marten;
+
+using System.Windows.Input;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public sealed record DeleteProductCommand(Guid Id):ICommand<DeleteProductResult>;
+
+public sealed record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var findProductById = await session.Query<Product>()
+            .FirstOrDefaultAsync(x => x.Id == command.Id)
+                ?? throw new ProductNotFoundException();
+
+            session.Delete(findProductById);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+
+        catch (Exception ex)
+        {
+            return new DeleteProductResult(false);
+        }
+
+        finally
+        {
+            await session.DisposeAsync();
+        }
+    }
+}
