39225db4f8d658f4970cbba740abe32989cc8165
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-04 15:49:47 +0330

    feat: Add Product Was Not Found Exception

---
+++ Src/Services/Catalog/Catalog.API/Exceptions/Products/ProductNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Exceptions.Products;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("Product Was Not Found")
+    {
+    }
+}
