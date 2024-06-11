dfefc58a43ea36f908a4f832b9e0b86a82176bf3
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-06-11 22:49:17 +0330

    feat: Add Validation Behaviors

--- Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,8 @@
 ﻿using BuildingBlocks.CQRS.Command;
 using Catalog.API.Models.Catalogs;
 
+using FluentValidation;
+
 using Marten;
 
 namespace Catalog.API.Products.CreateProduct;
@@ -9,23 +11,61 @@ public record CreateProductCommand(string Name, List<string> Categories,
         string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
+public sealed class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name Is Required");
+
+        RuleFor(x => x.Categories)
+            .NotEmpty()
+            .WithMessage("Category Is Requierd");
+
+        RuleFor(x => x.ImageFile)
+            .NotEmpty()
+            .WithMessage("Image Is Requierd");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+            .WithMessage("Price Must Be GreaterThan 0");
+    }
+}
+
 public sealed record CreateProductResult(Guid Id);
 
-internal sealed class CreateProductCommandHandler(IDocumentSession session)
+internal sealed class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand,
     CreateProductResult>
 {
-    public async Task<CreateProductResult> Handle(CreateProductCommand request,
+    public async Task<CreateProductResult> Handle(CreateProductCommand command,
         CancellationToken cancellationToken)
     {
+        //var validation = await validator
+        //    .ValidateAsync(command, cancellationToken);
+
+        //var errors = validation.Errors
+        //    .Select(x => x.ErrorMessage)
+        //    .ToList();
+        //if (errors.Any())
+        //{
+        //    string errorsText = default;
+        //    foreach (var item in errors)
+        //    {
+        //        errorsText += item + Environment.NewLine;
+        //    }
+        //    throw new ValidationException(errorsText);
+        //}
+
         Product newProduct = new Product()
         {
             Id = Guid.NewGuid(),
-            Name = request.Name,
-            Categories = request.Categories,
-            Description = request.Description,
-            ImageFile = request.ImageFile,
-            Price = request.Price
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
         };
 
         session.Store(newProduct);
--- Src/Services/Catalog/Catalog.API/Program.cs
+++ Src/Services/Catalog/Catalog.API/Program.cs
@@ -1,32 +1,42 @@
+using BuildingBlocks.Behaviors;
+
+using FluentValidation;
+
 using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddMediatR(configuration =>
+builder.Services
+    .AddMediatR(configuration =>
 {
     configuration.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    configuration.AddOpenBehavior(typeof(ValidationBehaviors<,>));
 });
 
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+builder.Services
+    .AddEndpointsApiExplorer();
+
+builder.Services
+    .AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",
         new() { Title = "Eshop Contact Api", Version = "1" });
 });
 
-builder.Services.AddMarten(configuration =>
+builder.Services
+    .AddMarten(configuration =>
 {
     configuration.Connection(builder.Configuration.GetConnectionString("DataBase")!);
 }).UseLightweightSessions();
 
+builder.Services
+    .AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 
-
-builder.Services.AddCarter();
-
-
+builder.Services
+    .AddCarter();
 
 var app = builder.Build();
 app.MapCarter();
