d4e9069ef4bc9350f19248d6bd397e4783b7a3f0
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-13 22:28:12 +0330

    feat: Add Structure in Basket

---
+++ Src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public sealed record GetBasketQuery(string userName) : IQuery<GetBasketResult>;
+
+public sealed record GetBasketResult(ShoppingCart Cart);
+    
+public class GetBasketQueryHandler:IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        return new(new ShoppingCart("test"));
+    }
+}
