53763f5b7602cae4871833720186cccd17a57b3a
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-14 09:53:29 +0330

    feat: Add StoreBasket

---
+++ Src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public sealed record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public sealed record StoreBasketResult(bool IsSuccess);
+
+public sealed class StockBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StockBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart Cannot Be Null;");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName Is Requierd");
+    }
+}
+
+public sealed class StoreBasketCommandHandler :
+    ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        return new StoreBasketResult(true);
+    }
+}
---
+++ Src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.StoreBasket;
+
+public sealed record StoreBasketRequest(ShoppingCart Cart);
+
+public sealed record StoreBasketResponse(bool Issuccess);
+
+public class StoreBasketEndpoint:ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/basket{response.Issuccess}", response);
+        })
+        .WithName("Store Basket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary(" Store Basket")
+        .WithDescription("Store Basket");
+    }
+}
