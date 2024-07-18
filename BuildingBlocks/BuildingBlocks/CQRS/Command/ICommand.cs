58dd014e07a7016784426d1fd6ed01bd731b6698
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-18 11:52:16 +0330

    faet: Add Build Block

---
+++ BuildingBlocks/BuildingBlocks/CQRS/Command/ICommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS.Command;
+
+public interface ICommand : ICommand<Unit>;
+
+public interface ICommand<out TResponse>:IRequest<TResponse>;
---
+++ BuildingBlocks/BuildingBlocks/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Exceptions;
+
+public class NotFoundException:Exception
+{
+    public NotFoundException(string entityName, object key) : base($"{entityName} By Id {key} Was Not Found")
+    {
+    }
+}
