6bc081b9ece2678bd4b6a31cc19fff2dfc5e2865
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-18 11:08:40 +0330

    feat: Add DeleteBasket Actions

---
+++ Src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.DeleteBasket;
+
+public sealed record DeleteBasketResponse(bool IsSuccess);
+
+public sealed class DeleteBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{username}", async (string username, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(username));
+
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        });
+    }
+}
---
+++ Src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public sealed record DeleteBasketCommand(string Username):ICommand<DeleteBasketResult>;
+public sealed record DeleteBasketResult(bool Issucces);
+
+public sealed class DeleteBasketCommandValidator :
+    AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.Username).NotEmpty().WithMessage("UserName Is Requierd");
+    }
+}
+
+public sealed class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        return new DeleteBasketResult(true);
+    }
+}
