f19d11a664a970a7eaefab6526b6192139ed6bca
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-18 11:54:02 +0330

    change

---
+++ Src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Models;
+
+public sealed class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => CalculateTotalSum();
+    
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    // For Mapping
+    public ShoppingCart()
+    {
+    }
+
+    public decimal CalculateTotalSum()
+    {
+        return Items.Sum(x => x.Price * x.Quantity);
+    }
+}
