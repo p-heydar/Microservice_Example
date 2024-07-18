733d5bdf1ccce3cf613be1ddbab796a8d105ce21
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-18 11:07:22 +0330

    feat: Add Contract and ConCreate BasketRepository

---
+++ Src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,28 @@
+﻿using Basket.API.Exceptions;
+using Marten;
+
+namespace Basket.API.Data;
+
+public class BasketRepository(IDocumentSession session):IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken)
+    {
+        var findBasket = await session.LoadAsync<ShoppingCart>(userName, cancellationToken)??throw new BasketNotFound(userName);
+        return findBasket;
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        session.Store(cart);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return cart;
+    }
+
+    public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken)
+    {
+        session.Delete<ShoppingCart>(userName);
+        await session.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
---
+++ Src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API.Data;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken);
+    Task<ShoppingCart> StoreBasketAsync(ShoppingCart cart, CancellationToken cancellationToken);
+    Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken);
+}
