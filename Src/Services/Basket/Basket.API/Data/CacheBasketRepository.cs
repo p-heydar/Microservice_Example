8a2c40ba646c8cb02a76f7ccf4a14c118058ea98
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-07-19 09:11:57 +0330

    feat: Implement CacheBasketRepository

---
+++ Src/Services/Basket/Basket.API/Data/CacheBasketRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.API.Data;
+
+public class CacheBasketRepository(IBasketRepository basketRepository, IDistributedCache cache)
+    :ICacheBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken)
+    {
+        #region Cache
+        var findFromCache = await cache.GetStringAsync(userName, cancellationToken);
+        if (findFromCache is not null)
+        {
+            return JsonSerializer.Deserialize<ShoppingCart>(findFromCache)!;
+        }
+        #endregion
+        else
+        {
+            var findFromDb = await basketRepository.GetBasketAsync(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(findFromDb), cancellationToken);
+
+            return findFromDb;
+        }
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // Put In Db
+        await basketRepository.StoreBasketAsync(cart, cancellationToken);
+        
+        // Put In Cache 
+        await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+        return cart;
+    }
+
+    public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken)
+    {
+        // Delete From Db
+        await basketRepository.DeleteBasketAsync(userName, cancellationToken);
+
+        // Remove From Cache
+        await cache.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+}
