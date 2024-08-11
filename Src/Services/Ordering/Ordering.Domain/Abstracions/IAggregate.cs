276055b92ce080e62e5dc7a8b3ab8e429b53d6d1
Pouya-Heydarabadi <Heydarabadip@gmail.com>
2024-08-11 21:59:53 +0330

    Feat: Add Ordering.Domain

---
+++ Src/Services/Ordering/Ordering.Domain/Abstracions/IAggregate.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstracions;
+
+public interface IAggregate<T> : IEntity<T>, IAggregate
+{
+    
+}
+
+public interface IAggregate:IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] clearDomainEvents();
+}
---
+++ Src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Ordering.Domain.ValueObjects;
+
+public record CustomerId
+{
+    public Guid value { get; }
+    private CustomerId(Guid value) => this.value = value;
+
+    public static CustomerId Of(Guid newValue)
+    {
+        ArgumentNullException.ThrowIfNull(newValue);
+        if (newValue == Guid.Empty)
+            throw new DomainException("CustomerId Cannot Be Empty!");
+        return new CustomerId(newValue);
+    }
+}
