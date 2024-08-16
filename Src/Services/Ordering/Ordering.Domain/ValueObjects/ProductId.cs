c60b75b753f09dbc28b3ece4dfafa200ee535da7
Pouya Heydarabadi <58113440+pouya-heydarabadi@users.noreply.github.com>
2024-08-16 23:29:02 +0330

    value object Change

--- Src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
+++ Src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
@@ -2,5 +2,14 @@
 
 public record ProductId
 {
-    public Guid value { get; }
+    public Guid Value { get; }
+    private ProductId(Guid value) => this.Value = value;
+
+    public static ProductId Of(Guid productId)
+    {
+        if (Guid.Empty == productId)
+            throw new DomainException("Product Id Cannot Be Empy Value!!");
+        
+        return new ProductId(productId);
+    }
 }
