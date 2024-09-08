11f37f2482859ff39dfc08cb3d795490a2c9508f
Pouya Heydarabadi <58113440+pouya-heydarabadi@users.noreply.github.com>
2024-09-08 19:42:26 +0330

    changes

---
+++ Src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IEntity<T>:IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+    // Create
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    
+    // Modify
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
---
+++ Src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureService(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("DataBase");
+
+        // Todo: Register EntityFrameWorkCore 
+        return services;
+    }
+}
